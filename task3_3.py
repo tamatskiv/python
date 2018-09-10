ddf4dfaf2ee63b0c6cd02c77229b44bbf8b33d2d
trybchynchuk <31851225+trybchynchuk@users.noreply.github.com>
2018-09-10 12:43:36 +0300

    Add files via upload

---
+++ task3_3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+height = input('Enter your height (in meters): ')
+weight = input('Enter your weight: ')
+
+bmi = float(weight) / (float(height)**2)
+
+print('Your BMI: ', bmi)
+
+
+
+
+
+
